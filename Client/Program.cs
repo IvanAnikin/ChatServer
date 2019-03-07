654447d888e1708d7ad4aac2314021bb244ad952
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-03-07 13:34:02 +0100

    Client part, a bit improved, lots to do

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Client
 {
     class Program
     {
+        /*TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+        string ipGateway = "127.0.0.1";
+        string nickname = "boy";
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+            NetworkStream serverStream = default(NetworkStream);
+            string ipGateway = "127.0.0.1";
+            string nickname = "boy";
+
+            connect(clientSocket, ipGateway, serverStream, nickname);
+
+            //create 'serverListener thread'
+
+            //'userListener' loop
         }
+
+        public static void connect(TcpClient clientSocket, string ipGateway, NetworkStream serverStream, string nickname)
+        {
+            Console.WriteLine("Connecting to chat... " + ipGateway);
+            clientSocket.Connect(ipGateway, 8888);
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(nickname);
+
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        public static void serverListener(NetworkStream serverStream, TcpClient clientSocket)
+        {
+            while (true)
+            {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            }
+        }
+
     }
 }
